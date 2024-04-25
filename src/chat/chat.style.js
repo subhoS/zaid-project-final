5329df4c20c567b22b71036e8cb9a3c5d53e4f30
Subhadeep Datta <subhadeepdatta2@gmail.com>
2024-04-25 13:03:07 +0530

    mc

---
+++ src/chat/chat.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const ChatView = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 40%;
+  padding: 10px;
+`;
